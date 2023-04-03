4f7d5d40e80448b78e42de4b2f9548c09e005d00
etctera <yilonglei790@gmail.com>
2023-04-03 22:37:37 +0800

    chore: login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
+import Login from "@/pages/login";
+
 function App() {
   return (
     <div className="App">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Login />
     </div>
   );
 }
